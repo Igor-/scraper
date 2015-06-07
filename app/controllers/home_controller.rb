5d0e83844853fb93b0442baa6903717c893d7429
Igor <i_666@bk.ru>
2015-06-07 17:08:24 +0300

    Remove garbage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,23 +4,10 @@ class HomeController < ApplicationController
     end
     def create
 
-      # doc = Nokogiri::HTML(open("https://selfsolve.apple.com/agreementWarrantyDynamic.do"))
-      # agent = Mechanize.new
-      # page = agent.get("https://selfsolve.apple.com/agreementWarrantyDynamic.do")
-      # form = page.form_with(id: "serialnumbercheck")
-      # form.fields.each { |f| pp f }
-      
-      # field = form.field_with!(name: 'sn')
-      # field.value = params[:warranty][:imei]
-      
-      # pp form.button_with(id: 'warrantycheckbutton')
-      # page = form.click_button
-      # pp page
       browser = Watir::Browser.new 
       browser.goto "https://selfsolve.apple.com/agreementWarrantyDynamic.do"
       browser.text_field(name: 'sn').set(params[:warranty][:imei])
       browser.button(id: "warrantycheckbutton").click
-
       html =  browser.html
       browser.close
 
@@ -33,9 +20,6 @@ class HomeController < ApplicationController
         expiration_date = ""
         status = :out_of_warranty
       end
-
-
-
             
       render json: {status: status, expiration_date: expiration_date}
     end
